c216c84f8b4b13d0ac828319d5132cea5ad37bfb
canvasplay <canvasplay1@gmail.com>
2017-03-09 18:21:47 +0100

    regex dynamic version display

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
               {
                 name: 'version',
                 search: '@version',
-                replace: '<%= pkg.version %>'
+                replace: '@<%= pkg.version %>'
               }
             ]
           }
